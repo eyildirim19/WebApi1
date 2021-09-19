c6a24ab675317ade75f495e2c29f854508d6a2f4
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2021-09-19 13:49:10 +0300

    Add project files.

---
+++ WebApi1/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi1.Controllers
+{
+    // Api sınıfının metotları dışarıya açık metotlardır. Entegratör olarak buradaki metotlarınıza karşı tarafın uygulamaları erişecektir... Webapi metotları HttpMetotlarıdır. GET,POST,PUT ve DELETE requestler ile çağrılırlar. 
+
+
+    // Apiler arayüz desteği olmadığı için testlerini yapabilmek adına client uygulamaları kullanılır. Bu uygulamar SOAP UI, Postman, apilerin içerisindeki swagger uygulamarlı kullanılır...Web Apilerimizi test edebilmek için postman uygulamasını indiriyoruz...Linki https://www.postman.com/downloads/
+    public class HomeController : ApiController
+    {
+        // IHttpActionResult => Http formatında result dönmemizi sağlar
+        public IHttpActionResult Get()
+        {
+            //return Ok() => Http Status Code formatında content döner...
+
+            // 200 => Success
+            // 400 => BadRequest
+            // 500 => Server Error
+            // 404 => Not Found
+
+            return Ok("Merhaba Arkadaşlar. Ben bir web apiyim. Benim diğer web uygulamalarından farkım ben arayüz sağlamam. Sadece uygulamara entegra olurum");
+        }
+
+        public IHttpActionResult Get(string isim)
+        {
+            if (isim == "Kaan")
+            {
+                return Ok("İşlem Başarılı");
+            }
+            else if (isim == "Öykü")
+            {
+                return NotFound();
+            }
+            else if (isim=="Ekrem")
+            {
+                return InternalServerError();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+    }
+}
---
+++ WebApi1/Controllers/OgrenciController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi1.Models;
+
+namespace WebApi1.Controllers
+{
+    public class OgrenciController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            List<Ogrenci> ogrencis = new List<Ogrenci>();
+            ogrencis.Add(new Ogrenci { Adi = "Kaan", SoyAdi = "Meşe" });
+            ogrencis.Add(new Ogrenci { Adi = "Ekrem", SoyAdi = "Yıldırım" });
+            ogrencis.Add(new Ogrenci { Adi = "Emre", SoyAdi = "Kayaoğlu" });
+
+            return Ok(ogrencis);
+        }
+
+        public IHttpActionResult Post(Ogrenci ogr)
+        {
+            return Ok("Kaydetme İşlemi Başarılıdır");
+        }
+
+    }
+}
